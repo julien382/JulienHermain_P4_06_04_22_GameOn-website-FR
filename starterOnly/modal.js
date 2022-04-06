77e65bd819207a1abe97f7d33504ff225b22ef17
julien382 <julhermain@gmail.com>
2022-04-06 21:56:32 +0200

    récup l'info de input 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,50 @@ function editNav() {
   }
 }
 
+
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const cross = document.querySelector(".close");
+
+//input
+const first = document.querySelector("#first");
+
+
+
+
+
+const closeModal = () => {
+  modalbg.classList.remove("appear");
+}
+
+// launch modal form
+const launchModal = () => {
+  modalbg.classList.add("appear");
+}
+
+// first input
+const handlerFirstInput = (event) => {
+  const value = event.target.value
+
+  if(value.length <= 2){
+    console.log("c'est pas bon")
+  } 
+  else{
+    console.log("c'est bon")
+  }
+}
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+// close modal event
+cross.addEventListener("click", closeModal);
 
-
+// first modal event
+first.addEventListener("input", handlerFirstInput);
