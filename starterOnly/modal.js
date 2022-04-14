6f891bc6b4fe2d9b71d518a0f3c542f34eae8480
julien382 <julhermain@gmail.com>
2022-04-14 14:01:37 +0200

    js avec bastien de la visio

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,6 +9,41 @@ function editNav() {
 
 /////////////////////////////////////
 
+let USER_INPUT = {
+  firstname: {
+    value: null,
+    validate: false
+  },
+  lastname: {
+    value: null,
+    validate: false
+  },
+  email: {
+    value: null,
+    validate: false
+  },
+  birthdate: {
+    value: null,
+    validate: false
+  },
+  tournament: {
+    value: null,
+    validate: false
+  },
+  city: {
+    value: null,
+    validate: false
+  },
+  cgu: {
+    value: null,
+    validate: false
+  },
+  newsletter: {
+    value: null,
+    validate: false
+  },
+}
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -17,6 +52,7 @@ const formData = document.querySelectorAll(".formData");
 const cross = document.querySelector(".close");
 
 //input
+const form = document.querySelector("form");
 const first = document.querySelector("#first");
 const last = document.querySelector("#last");
 const email = document.querySelector("#email");
@@ -38,13 +74,17 @@ const launchModal = () => {
 
 // input input
 const handlerFirstInput = (event) => {
+  // const value = first.value
   const value = event.target.value
+  USER_INPUT.firstname.value = value
 
   if(value.length <= 2){
     console.log("prénom c'est pas bon")
+    USER_INPUT.firstname.validate = false
   } 
   else{
     console.log("prénom c'est bon")
+    USER_INPUT.firstname.validate = true
   }
 }
 
@@ -61,14 +101,14 @@ const lastInput = (event) => {
 
 const emailInput = (event) => {
   const value = event.target.value
+  const emailRegex = new RegExp("^[a-z0-9\.]+@[a-z0-9]+\.[a-z]{2,3}$", 'g') // regex maison pour les email
 
-  /*const emailRegex = new RegExp/*/
-
-  if(emailRegex.test(value)){
-    console.log("email c'est pas bon")
-  } 
-  else{
+  if (emailRegex.test(value)){
     console.log("email c'est bon")
+  } 
+  else {
+    //
+    console.log("email c'est pas bon")
   }
 }
 
@@ -84,17 +124,30 @@ const birthdateInput = (event) => {
 }
 
 const quantityInput = (event) => {
+  const numberRegex = new RegExp("^[0-9]+$", 'g') // le + ==> au moins 1 caractère (ici chiffre)
   const value = event.target.value
 
-  if(value.length == 0){
-    console.log("quantity c'est pas bon")
-  } 
-  else{
+  if (numberRegex.test(value)) {
     console.log("quantity c'est bon")
+  } else {
+    console.log("quantity c'est pas bon");
   }
+
 }
 
 
+
+const validate = (event) => {
+  // on arrête le comportement par défaut
+  event.preventDefault()
+  // a partir de là c'est bibi qui gère !
+  console.log('user responses:', USER_INPUT);
+
+  // if toute les clé de USER_INPUT.xxx.validate sont bonnes
+  // console.log('validate: on peut envoyer les datas')
+  // else
+  // console.log('pas validate')
+}
 
 
 /////////////////////////////////////
@@ -105,6 +158,8 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 cross.addEventListener("click", closeModal);
+
+form.addEventListener('submit', validate)
 
 // input modal event
 first.addEventListener("input", handlerFirstInput);
